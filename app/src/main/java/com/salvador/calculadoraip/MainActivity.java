8477be7007cb55465797caadaab0e8cec7a8e086
salvadorc94 <secg.1994@gmail.com>
2018-04-07 14:50:13 -0600

    Arreglo pequeños de interfaz.

--- app/src/main/java/com/salvador/calculadoraip/MainActivity.java
+++ app/src/main/java/com/salvador/calculadoraip/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void reset(View v){
         ip.setText("");
+        ip.requestFocus();
         mascara.setText("");
         idnet.setText("");
         broadcast.setText("");
